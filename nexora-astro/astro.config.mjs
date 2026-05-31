a02bb16ed0103e4c0017354b80d52885fb2c1eef
Saishree <109239386+repl-venkata-saishree@users.noreply.github.com>
2026-05-31 16:33:14 +0530

    fix issues

--- nexora-astro/astro.config.mjs
+++ nexora-astro/astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  server: {
-  host: '0.0.0.0',
-  port: 10000,
-},
 });
